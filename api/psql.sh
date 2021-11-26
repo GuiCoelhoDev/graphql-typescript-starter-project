817ed759da297bd45a079aa776f3cafd4f51c8e4
Thales Menezes <thlmenezes@pm.me>
2021-11-26 03:29:23 -0300

    🔨 add docker volume to persist data

--- api/psql.sh
+++ api/psql.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-docker run --rm -p 5432:5432 --name graphql-postgres --env-file .env postgres
+
+docker run --rm -p 5432:5432 \
+  --name graphql-postgres \
+  --env-file .env \
+  -v graphql_postgres:/var/lib/postgresql/data \
+  postgres
