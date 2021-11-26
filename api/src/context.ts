00bec175dcfba11ab40dc600c8e0d11f4938fb61
Thales Menezes <thlmenezes@pm.me>
2021-11-26 03:33:07 -0300

    ✨ add http req to context

--- api/src/context.ts
+++ api/src/context.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { Request } from "@types/express"
 
 const prisma = new PrismaClient();
 
 export interface Context {
   prisma: PrismaClient;
+  req: Request
 }
 
-export function createContext(): Context {
+export function createContext(req: Request): Context {
   return {
+    req,
     prisma,
   };
 }
