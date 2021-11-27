18795f19df6e36689c97e74054e1d87736e60961
Thales Menezes <thlmenezes@pm.me>
2021-11-26 23:02:41 -0300

    ✨ add password column

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -6,14 +6,17 @@ const userData: Prisma.UserCreateInput[] = [
   {
     name: "Alice",
     email: "alice@prisma.io",
+    password: "$2a$10$TLtC603wy85MM./ot/pvEec0w2au6sjPaOmLpLQFbxPdpJH9fDwwS", // myPassword42
   },
   {
     name: "Nilu",
     email: "nilu@prisma.io",
+    password: "$2a$10$k2rXCFgdmO84Vhkyb6trJ.oH6MYLf141uTPf81w04BImKVqDbBivi", // random42
   },
   {
     name: "Mahmoud",
     email: "mahmoud@prisma.io",
+    password: "$2a$10$lTlNdIBQvCho0BoQg21KWu/VVKwlYsGwAa5r7ctOV41EKXRQ31ING", // iLikeTurtles42
   },
 ];
 
--- api/src/graphql/User.ts
+++ api/src/graphql/User.ts
@@ -1,4 +1,5 @@
 import { Context } from "@starter-project/api/context";
+import { hash } from "bcryptjs";
 import {
   arg,
   inputObjectType,
@@ -12,11 +13,12 @@ import {
 export const User = objectType({
   name: "User",
   definition(t) {
-    t.nonNull.int("id", { description: 'Unique identifier for the resource' });
-    t.string("name", { description: 'User\'s name' });
-    t.nonNull.string("email", { description: 'User\'s best email address' });
+    t.nonNull.int("id", { description: "Unique identifier for the resource" });
+    t.string("name", { description: "User's name" });
+    t.nonNull.string("email", { description: "User's best email address" });
+    t.nonNull.string("password", { description: "User's safest random generated password" });
   },
-  description: 'Represents a person'
+  description: "Represents a person",
 });
 
 export const queryAllUsers = queryField((t) => {
@@ -25,9 +27,9 @@ export const queryAllUsers = queryField((t) => {
     resolve: (_parent, _args, context: Context) => {
       return context.prisma.user.findMany();
     },
-    description: 'Fetches All Users from db'
+    description: "Fetches All Users from db",
   });
-})
+});
 
 export const queryOneUser = queryField((t) => {
   t.field("user", {
@@ -44,7 +46,7 @@ export const queryOneUser = queryField((t) => {
         },
       });
     },
-    description: 'Fetches One User from db using any of its field as search key'
+    description: "Fetches One User from db using any of its field as search key",
   });
 });
 
@@ -54,10 +56,11 @@ export const createUser = mutationField((t) => {
     args: {
       data: nonNull(arg({ type: UserCreateInput })),
     },
-    resolve: (_parent, args, context: Context) => {
-      return context.prisma.user.create({ data: args.data });
+    resolve: async (_parent, args, context: Context) => {
+      const hashedPassword = await hash(args.data.password, 10);
+      return context.prisma.user.create({ data: { ...args.data, password: hashedPassword } });
     },
-    description: 'Creates One User on db'
+    description: "Creates One User on db",
   });
 });
 
@@ -68,14 +71,16 @@ export const patchUser = mutationField((t) => {
       id: intArg(),
       data: nonNull(arg({ type: UserUpdateInput })),
     },
-    resolve: (_parent, args, context: Context) => {
+    resolve: async (_parent, args, context: Context) => {
+      const password = args.data.password ? await hash(args.data.password, 10) : undefined;
       return context.prisma.user.update({
         where: {
-          id: args.id || undefined,
+          id: args.id ?? undefined,
         },
         data: {
           name: args.data.name,
           email: args.data.email ?? undefined,
+          password,
         },
       });
     },
@@ -103,8 +108,9 @@ export const UserCreateInput = inputObjectType({
   definition(t) {
     t.string("name");
     t.nonNull.string("email");
+    t.nonNull.string("password");
   },
-  description: 'Type definition for creating a User'
+  description: "Type definition for creating a User",
 });
 
 export const UserUpdateInput = inputObjectType({
@@ -112,8 +118,9 @@ export const UserUpdateInput = inputObjectType({
   definition(t) {
     t.string("name");
     t.string("email");
+    t.string("password");
   },
-  description: 'Type definition for updating a User'
+  description: "Type definition for updating a User",
 });
 
 export const UserWhereInput = inputObjectType({
@@ -123,7 +130,7 @@ export const UserWhereInput = inputObjectType({
     t.string("name");
     t.string("email");
   },
-  description: 'Type definition for searching (where) a User'
+  description: "Type definition for searching (where) a User",
 });
 
 export const userTypes = {
