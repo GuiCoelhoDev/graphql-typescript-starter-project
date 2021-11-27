6ac34f5c1cf1085b45c6ace432a7767e0ce59113
Thales Menezes <thlmenezes@pm.me>
2021-11-26 23:10:59 -0300

    ✨ add new rules

--- api/src/permissions/index.ts
+++ api/src/permissions/index.ts
@@ -1,21 +1,47 @@
-import { allow, rule, shield } from 'graphql-shield'
-import { getUserId } from '../utils'
-import { Context } from '../context'
+import { AuthenticationError, UserInputError } from "apollo-server";
+import { compare } from "bcryptjs";
+import { allow, rule, shield } from "graphql-shield";
+
+import { Context } from "../context";
+import { getUserId } from "../utils";
+
+function findUser(email: string, context: Context) {
+  return context.prisma.user.findUnique({
+    where: { email },
+  });
+}
 
 const rules = {
   isAuthenticatedUser: rule()((_parent, _args, context: Context) => {
-    const userId = getUserId(context)
-    return Boolean(userId)
+    const userId = getUserId(context);
+    return Boolean(userId);
   }),
-}
+  userAlreadyExists: rule()(async (_parent, args, context: Context) => {
+    const user = Boolean(await findUser(args.data.email, context));
+    return !user || new UserInputError(`User already found for email: ${args.data.email}`);
+  }),
+  userNotFound: rule()(async (_parent, args, context: Context) => {
+    const user = Boolean(await findUser(args.data.email, context));
+    return user || new AuthenticationError(`No user found for email: ${args.data.email}`);
+  }),
+  invalidPassword: rule()(async (_parent, args, context: Context) => {
+    const user = await findUser(args.data.email, context);
+    const passwordValid = await compare(args.data.password, user?.password ?? "");
+    return passwordValid || new AuthenticationError("Invalid password");
+  }),
+};
 
-export const permissions = shield({
-  Query: {
-    "allUsers": allow
+export const permissions = shield(
+  {
+    Query: {
+      allUsers: allow,
+    },
+    Mutation: {
+      "*": allow,
+      createUser: rules.userAlreadyExists,
+    },
   },
-  Mutation: {
-    "*": allow
+  {
+    fallbackRule: allow,
   }
-}, {
-  fallbackRule: allow
-})
+);
